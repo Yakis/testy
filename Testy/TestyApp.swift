d22be605075cde8ca29f583e5841263a9bc5f8fc
Mugurel Moscaliuc <mugur.moscaliuc@gmail.com>
2021-07-13 13:29:01 +0100

    Initial Commit

---
+++ Testy/TestyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestyApp.swift
+//  Testy
+//
+//  Created by Mugurel Moscaliuc on 13/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
